1b1ca78bcf8bc3a427968171163371e48d9da4c0
Magdalena961 <ma82hk@gmail.com>
2025-05-06 13:33:37 +0000

    7

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import datetime
-pip install plotly
 import plotly.express as px  # Zaimportowanie Plotly
 
 st.set_page_config(page_title="FoodWasteApp", layout="wide")
